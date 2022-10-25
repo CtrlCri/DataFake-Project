0d6144125e1b66a26c8e30c8941ec3879d62c8fb
armycrih <armycrih@gmail.com>
2022-10-25 19:33:54 -0300

    database config.

--- datafake/datafake/settings.py
+++ datafake/datafake/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'datafake.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'DATABASE_PORT': os.getenv('DATABASE_PORT')
     }
 }
 
