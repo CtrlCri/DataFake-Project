8297f05ecb38d27f3de9c956759076d4d5318dad
armycrih <armycrih@gmail.com>
2022-10-26 12:49:31 -0300

    first template

--- datafake/dataset/views.py
+++ datafake/dataset/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+from .models import Dataset
+
 def index(request):
-    return HttpResponse("this is the home")
+    latest_dataset_list = Dataset.objects.all()
+    return render(request, "dataset/index.html", {
+        "latest_dataset_list": latest_dataset_list
+    })
 
 def detail(request, id):
     return HttpResponse(f"Estás viendo el dataset número {id}")
