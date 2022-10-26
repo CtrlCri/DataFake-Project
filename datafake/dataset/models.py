a82a846e7bb6726f0a71a57bf270f693629f05a3
armycrih <armycrih@gmail.com>
2022-10-26 00:34:11 -0300

    first migrations

--- datafake/dataset/models.py
+++ datafake/dataset/models.py
@@ -1,19 +1,23 @@
+import datetime
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 class Dataset(models.Model):
     title = models.CharField(max_length=80)
-    slug_title = models.SlugField
-
     code = models.TextField(blank=False)
     about = models.TextField(blank=False)
-    
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    
     num_downloads = models.IntegerField(default=0)
 
     posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.created_at >= timezone.now() - datetime.timedelta(days=1)
 
 class Like(models.Model):
     user =models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
