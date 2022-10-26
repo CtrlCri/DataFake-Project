509c68bfa60ec5acc3a1138fda1e303d2fb70148
armycrih <armycrih@gmail.com>
2022-10-26 12:32:20 -0300

    first views

--- datafake/dataset/views.py
+++ datafake/dataset/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Crih!")
+    return HttpResponse("this is the home")
+
+def detail(request, id):
+    return HttpResponse(f"Estás viendo el dataset número {id}")
